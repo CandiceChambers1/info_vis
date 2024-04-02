d77169198397ffa2830843110446f6567a5e1d9d
candice <chambersc2017@my.fit.edu>
2024-04-02 16:09:44 -0400

    Edit index.html

---
+++ src/noun_extracter.py
@@ -0,0 +1,20 @@
+from flair.data import Sentence
+from flair.models import SequenceTagger
+
+# load tagger
+tagger = SequenceTagger.load("flair/chunk-english")
+
+# make example sentence
+sentence = Sentence("The happy man has been eating at the diner")
+
+# predict NER tags
+tagger.predict(sentence)
+
+# print sentence
+print(sentence)
+
+# print predicted NER spans
+print('The following NER tags are found:')
+# iterate over entities and print
+for entity in sentence.get_spans('np'):
+    print(entity)
